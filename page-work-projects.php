41856cee2a9327e30cb837e0b5697294074f0ee8
emersonconnors <109981890+emersonconnors@users.noreply.github.com>
2022-11-03 00:10:00 -0700

    isotope added in footer, page-work, styles

--- page-work-projects.php
+++ page-work-projects.php
@@ -23,8 +23,23 @@ get_header();
 	while (have_posts()) :
 		the_post();
 		get_template_part('template-parts/content', 'page');
+	?>
+		<div class="button-group filter-button-group">
+			<button data-filter="*">Show All</button>
+			<?php
+			$terms = get_terms(array(
+				'taxonomy' => 'work-languages',
+				'hide_empty' => false,
+			));
+			foreach ($terms as $term) {
+				echo '<button data-filter=".' . $term->slug . '">' . $term->name . '</button>';
+			}
+			?>
+		</div>
 
 
+		<?php
+		echo '<div class="grid">';
 		$args = array(
 			'post_type'      => 'projects-work',
 			'posts_per_page' => -1
@@ -35,16 +50,27 @@ get_header();
 		if ($query->have_posts()) {
 			while ($query->have_posts()) {
 				$query->the_post();
-	?>
-				<h4 class="works-h4"><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h4>
 
 
-	<?php
+		?>
+				<div class="grid-item <?php $terms = wp_get_post_terms(get_the_id(), $taxonomy = 'work-languages');
+										foreach ($terms as $term) {
+											echo $term->slug . ' ';
+										} ?>">
+
+
+
+					<h4 class="works-h4"><a href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h4>
+
+
+		<?php
 				echo '<div class="textarea-excerpt">' . custom_field_excerpt() . '</div>';
+
+				echo '</div>';
 			}
 			wp_reset_postdata();
 		}
-
+		echo '</div>';
 
 		// If comments are open or we have at least one comment, load up the comment template.
 		if (comments_open() || get_comments_number()) :
@@ -52,12 +78,12 @@ get_header();
 		endif;
 
 	endwhile; // End of the loop.
-	?>
-	<?php
+		?>
+		<?php
 
 
 
-	?>
+		?>
 
 </main><!-- #main -->
 
